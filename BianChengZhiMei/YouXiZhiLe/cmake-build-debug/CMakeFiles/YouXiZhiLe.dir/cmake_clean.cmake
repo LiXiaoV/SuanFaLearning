ef7c2fbb765c48ee4a248ff190bd4090006868b9
xiaoV <1011529903@qq.com>
2018-12-30 22:47:55 +0800

    add a directory named 编程之美

---
+++ BianChengZhiMei/YouXiZhiLe/cmake-build-debug/CMakeFiles/YouXiZhiLe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YouXiZhiLe.dir/main.c.o"
+  "YouXiZhiLe.pdb"
+  "YouXiZhiLe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/YouXiZhiLe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
